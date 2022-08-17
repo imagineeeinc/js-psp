bfbf112d45b2dfe9775f8884d610a07597392f29
Imagineee <imagineeelego@gmail.com>
2022-08-17 12:58:50 +0100

    added base files

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![no_std]
+#![no_main]
+
+// Create a module named "sample_module" with version 1.0
+psp::module!("sample_module", 1, 0);
+
+fn psp_main() {
+    psp::enable_home_button();
+    psp::dprintln!("Hello PSP from rust!");
+}
